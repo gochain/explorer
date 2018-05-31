581d99d1b99a822699e39826e9e599bdcc579b47
Roman Kononov <rkononov@gmail.com>
2018-05-31 20:28:41 +0600

    Added gcr support

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ set -exuo pipefail
 
 user="gochain"
 image="explorer"
-
+gcr_project="gochain-core"
 # ensure working dir is clean
 git status
 if [[ -z $(git status -s) ]]
@@ -15,10 +15,14 @@ else
 fi
 
 version_file="package.json"
+docker create -v /data --name file alpine /bin/true
+docker cp ${HOME}/project/$version_file file:/data/$version_file
 # Bump version, patch by default - also checks if previous commit message contains `[bump X]`, and if so, bumps the appropriate semver number - https://github.com/treeder/dockers/tree/master/bump
-docker run --rm -it -v $PWD:/app -w /app treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
+docker run --rm -it --volumes-from file -w / treeder/bump --filename /data/$version_file "$(git log -1 --pretty=%B)"
+docker cp file:/data/$version_file $version_file
 version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
 echo "Version: $version"
+
 
 docker build . -t $user/$image
 git add -u
@@ -31,3 +35,11 @@ git push origin $version
 docker tag $user/$image:latest $user/$image:$version
 docker push $user/$image:$version
 docker push $user/$image:latest
+
+#Push docker images to GCR
+gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
+docker login -u _json_key -p "$(cat ${HOME}/gcloud-service-key.json)" https://gcr.io
+docker tag $user/$image:latest gcr.io/$gcr_project/$image:latest
+docker tag $user/$image:latest gcr.io/$gcr_project/$image:$version
+docker push gcr.io/$gcr_project/$image:latest
+docker push gcr.io/$gcr_project/$image:$version
--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ docker run --name test_mongo -d --network="container:$varA" mongo
 docker build -t gochain/explorer:test_ci .
 docker run --name test_explorer -d --network="container:$varA" -e RPC_HOST=localhost  gochain/explorer:test_ci
 sleep 5 # let's wait until server start
-npm test
+docker exec test_explorer npm test
 for imagename in test_web3 test_mongo test_explorer; do
     echo "* Trying to stop ${imagename}..."
     [ -z $(docker ps -a -f "name=$imagename" --format '{{.Names}}') ] || docker rm -f $imagename
