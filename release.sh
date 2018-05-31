b4c4093e87f058bcce4b60e8cb9a5a01e8d9bd39
Roman Kononov <rkononov@gmail.com>
2018-05-31 21:01:04 +0600

    gcr support

--- release.sh
+++ release.sh
@@ -4,33 +4,33 @@ set -exuo pipefail
 user="gochain"
 image="explorer"
 gcr_project="gochain-core"
-# # ensure working dir is clean
-# git status
-# if [[ -z $(git status -s) ]]
-# then
-#   echo "tree is clean"
-# else
-#   echo "tree is dirty, please commit changes before running this"
-#   exit 1
-# fi
+# ensure working dir is clean
+git status
+if [[ -z $(git status -s) ]]
+then
+  echo "tree is clean"
+else
+  echo "tree is dirty, please commit changes before running this"
+  exit 1
+fi
 
 version_file="package.json"
-# # Bump version, patch by default - also checks if previous commit message contains `[bump X]`, and if so, bumps the appropriate semver number - https://github.com/treeder/dockers/tree/master/bump
-# docker run --rm -it -v $PWD:/app -w /app treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
+# Bump version, patch by default - also checks if previous commit message contains `[bump X]`, and if so, bumps the appropriate semver number - https://github.com/treeder/dockers/tree/master/bump
+docker run --rm -it -v $PWD:/app -w /app treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
 version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
-# echo "Version: $version"
+echo "Version: $version"
 
 docker build . -t $user/$image
-# git add -u
-# git commit -m "$image: $version release [skip ci]"
-# git tag -f -a "$version" -m "version $version"
-# git push
-# git push origin $version
+git add -u
+git commit -m "$image: $version release [skip ci]"
+git tag -f -a "$version" -m "version $version"
+git push
+git push origin $version
 
-# # Finally, push docker images
-# docker tag $user/$image:latest $user/$image:$version
-# docker push $user/$image:$version
-# docker push $user/$image:latest
+# Finally, push docker images
+docker tag $user/$image:latest $user/$image:$version
+docker push $user/$image:$version
+docker push $user/$image:latest
 
 #Push docker images to GCR
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
