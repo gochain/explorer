b9718ebedc9bd91280cb986045d809d608ab3d18
Roman Kononov <rkononov@gmail.com>
2018-06-04 23:10:42 +0600

    gcr support

--- release.sh
+++ release.sh
@@ -16,7 +16,11 @@ gcr_project="gochain-core"
 
 version_file="package.json"
 # Bump version, patch by default - also checks if previous commit message contains `[bump X]`, and if so, bumps the appropriate semver number - https://github.com/treeder/dockers/tree/master/bump
-docker run --rm -it -v $PWD:/app -w /app treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
+docker create -v /params --name file alpine /bin/true
+docker cp $version_file file:/$version_file
+# Bump version, patch by default - also checks if previous commit message contains `[bump X]`, and if so, bumps the appropriate semver number - https://github.com/treeder/dockers/tree/master/bump
+docker run --rm -it --volumes-from file -w / treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
+docker cp file:/$version_file $version_file
 version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
 echo "Version: $version"
 
