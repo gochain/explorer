f18592eea483a6b256b206352bfbaff382c9efd7
Anthony Ettinger <anthony@chovy.com>
2018-07-04 06:38:28 -0700

    add pagination to rich list

--- public/js/controllers/RIchlistController.js
+++ public/js/controllers/RIchlistController.js
@@ -5,19 +5,36 @@ angular.module('BlocksApp').controller('RichlistController', function(
     $http
 ) {
     $rootScope.$state.current.data["pageSubTitle"] = $stateParams.hash;
-    $scope.richlist = [];
+    $scope.richlist = {
+        rankings: []
+    };
+    $scope.limit = 10;
+    $scope.start = 0;
+    $scope.isMoreDisabled = false;
 
-    $http.get('/api/richlist?start=0&limit=100')
-        .then(res => {
-            $scope.richlist = res.data;
-            $scope.richlist.rankings = $scope.richlist.rankings.map(acct => {
-                acct.supplyOwned = supplyOwned(acct);
+    $scope.getMore = () => {
+        $http.get(`/api/richlist?start=${$scope.start}&limit=${$scope.limit}`)
+            .then(res => {
+                $scope.richlist.rankings = $scope.richlist.rankings.concat(res.data.rankings);
+                $scope.richlist.circulatingSupply = res.data.circulatingSupply;
+                $scope.richlist.totalSupply = res.data.totalSupply;
+                $scope.start += 10;
 
-                return acct;
-            })
+                if (res.data.rankings.length < $scope.limit) {
+                    $scope.isMoreDisabled = true;
+                }
+
+                getSupplyOwned();
+            });
+    };
+
+    $scope.getMore();
+
+    function getSupplyOwned () {
+        $scope.richlist.rankings = $scope.richlist.rankings.map(acct => {
+            acct.supplyOwned = (acct.balance / $scope.richlist.circulatingSupply * 100).toFixed(2);
+
+            return acct;
         });
-
-    function supplyOwned (account) {
-        return (account.balance / $scope.richlist.circulatingSupply * 100).toFixed(2)
     }
 })
