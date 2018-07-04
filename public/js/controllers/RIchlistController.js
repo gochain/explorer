6f043d9b1974b0823b5740bd11a5c91032f939e3
Anthony Ettinger <anthony@chovy.com>
2018-07-03 21:27:07 -0700

    load data from api

--- public/js/controllers/RIchlistController.js
+++ public/js/controllers/RIchlistController.js
@@ -5,10 +5,19 @@ angular.module('BlocksApp').controller('RichlistController', function(
     $http
 ) {
     $rootScope.$state.current.data["pageSubTitle"] = $stateParams.hash;
-    $scope.accounts = [];
+    $scope.richlist = [];
 
     $http.get('/api/richlist?start=0&limit=100')
         .then(res => {
-            $scope.accounts = res.data;
+            $scope.richlist = res.data;
+            $scope.richlist.rankings = $scope.richlist.rankings.map(acct => {
+                acct.supplyOwned = supplyOwned(acct);
+
+                return acct;
+            })
         });
+
+    function supplyOwned (account) {
+        return (account.balance / $scope.richlist.circulatingSupply * 100).toFixed(2)
+    }
 })
