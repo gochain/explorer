838ab3e844a905b9e5222ae14b2c3b0d47c7d9f2
r-kononov <rkononov@gochain.io>
2018-09-07 23:55:42 +0600

    trying to fix CI

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,6 @@
 set -e
-docker run --name test_web3 -d -p 8545:8545 -p 8080:8080 -p 27017:27017 trufflesuite/ganache-cli
+docker run --name test_mongo -d -p 8545:8545 -p 8080:8080 -p 27017:27017 mongo
 varA=`docker ps --no-trunc -q | cut -c 1-12`
-docker run --name test_mongo -d --network="container:$varA" mongo
 docker build -t gochain/explorer:test_ci .
 docker run --name test_explorer_grabber -d --network="container:$varA" gochain/explorer:test_ci grabber -u https://testnet-rpc.gochain.io -s 10
 docker run --name test_explorer_server -d --network="container:$varA" gochain/explorer:test_ci server -d /explorer/ -u https://testnet-rpc.gochain.io
@@ -12,9 +11,9 @@ docker logs test_explorer_grabber
 echo "Docker logs for server"
 docker logs test_explorer_server
 echo "Trying curl"
-curl -f http://localhost:8080/
-curl -f http://localhost:8080/api/blocks/10
-curl -f http://localhost:8080/api/blocks/10/transactions
+docker run --rm --network="container:$varA" byrnedo/alpine-curl -f http://localhost:8080/
+docker run --rm --network="container:$varA" byrnedo/alpine-curl -f http://localhost:8080/api/blocks/10 
+docker run --rm --network="container:$varA" byrnedo/alpine-curl -f http://localhost:8080/api/blocks/10/transactions
 for imagename in test_web3 test_mongo test_explorer_grabber test_explorer_server; do
     echo "* Trying to stop ${imagename}..."
     [ -z $(docker ps -a -f "name=$imagename" --format '{{.Names}}') ] || docker rm -f $imagename
