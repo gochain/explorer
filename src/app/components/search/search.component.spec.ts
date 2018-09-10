b9a80b3cd9a66dd3ea797cd85aae300423289b95
Bergauz <bergauzhrm@gmail.com>
2018-09-10 21:41:49 +0600

    @front #additional component's tests #2

---
+++ src/app/components/search/search.component.spec.ts
@@ -0,0 +1,28 @@
+/*CORE*/
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+/*MODULES*/
+import {AppModule} from '../../app.module';
+/*COMPONENTS*/
+import {SearchComponent} from './search.component';
+
+describe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
