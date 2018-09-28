7912f86b43b17e98f6f23720f281e658a6b99fb2
r-kononov <rkononov@gochain.io>
2018-09-28 20:58:21 +0600

    fixed nonce for blocks, set name

--- server/backend/db_backend.go
+++ server/backend/db_backend.go
@@ -96,7 +96,7 @@ func (self *MongoBackend) parseBlock(block *types.Block) *models.Block {
 		ParentHash: block.ParentHash().Hex(),
 		TxHash:     block.Header().TxHash.Hex(),
 		GasUsed:    strconv.Itoa(int(block.Header().GasUsed)),
-		Nonce:      string(block.Nonce()),
+		Nonce:      strconv.Itoa(int((block.Nonce()))),
 		Miner:      block.Coinbase().Hex(),
 		TxCount:    int(uint64(len(block.Transactions()))),
 		Difficulty: block.Difficulty().Int64(),
