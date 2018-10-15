f67d04ea062ad658caa0738f657349b0a48d806d
Bergauz <bergauzhrm@gmail.com>
2018-10-15 21:11:47 +0600

    @front #fix removing period when evrthg is zero after it

--- src/app/pipes/wei_to_go.ts
+++ src/app/pipes/wei_to_go.ts
@@ -7,6 +7,7 @@ import {PipeTransform, Pipe} from '@angular/core';
 export class WeiToGOPipe implements PipeTransform {
 
   transform(val: string, showUnit: boolean = true, removeTrailingZeros: boolean = null): string {
+    console.log(val);
     const moveTo = 18;
     const parts = val.toString().split('.');
     if (parts[0].length > moveTo) {
@@ -19,6 +20,7 @@ export class WeiToGOPipe implements PipeTransform {
     if (removeTrailingZeros) {
       // replace trailing zeros with exact amount of spaces
       value = value.replace(/0(?=(0+$|$))/g, ` `);
+      value = value.replace(/\.(?=\s)/g, ` `);
     } else {
       // delete trailing zeros
       value = value.replace(/\.?0+$/, '');
