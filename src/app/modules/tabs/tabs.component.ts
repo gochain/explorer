1339847b7e321331040e80efd91f074d62f17eff
Bergauz <bergauzhrm@gmail.com>
2018-10-25 16:43:30 +0600

    @front #new linkable tabs, closes #147

--- src/app/modules/tabs/tabs.component.ts
+++ src/app/modules/tabs/tabs.component.ts
@@ -1,6 +1,10 @@
-import {AfterContentInit, Component, ContentChildren, QueryList} from '@angular/core';
-import {TabComponent} from './components/tab/tab.component';
+/*CORE*/
+import {AfterContentInit, Component, ContentChildren, Input, OnInit, QueryList} from '@angular/core';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {Subscription} from 'rxjs';
+/*COMPONENTS*/
+import {TabComponent} from './components/tab/tab.component';
+/*UTILS*/
 import {AutoUnsubscribe} from '../../decorators/auto-unsubscribe';
 
 @Component({
@@ -9,16 +13,35 @@ import {AutoUnsubscribe} from '../../decorators/auto-unsubscribe';
   styleUrls: ['./tabs.component.scss']
 })
 @AutoUnsubscribe('_subsArr$')
-export class TabsComponent implements AfterContentInit {
+export class TabsComponent implements OnInit, AfterContentInit {
+  @Input() name: string;
   @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
   activeTab: TabComponent;
+
+  private _initialTabName: string;
   private _subsArr$: Subscription[] = [];
+
+  constructor(private _activatedRoute: ActivatedRoute, private _router: Router) {
+  }
+
+  ngOnInit() {
+    this._subsArr$.push(this._activatedRoute.queryParamMap.subscribe((params: ParamMap) => {
+      if (params.has(this.name)) {
+        this._initialTabName = params.get(this.name);
+      }
+    }));
+  }
 
   ngAfterContentInit() {
     this._subsArr$.push(this.tabs.changes.subscribe(this.onTabsChange));
     // Asynchronous update preventing ExpressionChangedAfterItHasBeenCheckedError
     setTimeout(() => {
-      this.activeTab = this.tabs.first;
+      if (this._initialTabName) {
+        const activeTab = this.tabs.find((tab: TabComponent) => tab.name === this._initialTabName) || this.tabs.first;
+        this.onTabSelect(activeTab);
+      } else {
+        this.onTabSelect(this.tabs.first);
+      }
     });
   }
 
@@ -31,9 +54,16 @@ export class TabsComponent implements AfterContentInit {
     } else {
       this.activeTab = null;
     }
-  };
+  }
 
   onTabSelect(tab: TabComponent) {
     this.activeTab = tab;
+    this._router.navigate([], {
+      relativeTo: this._activatedRoute,
+      queryParams: {
+        ...this._activatedRoute.snapshot.queryParams,
+        [this.name]: tab.name,
+      }
+    });
   }
 }
