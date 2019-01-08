15780c0532a7b620f6f6dfed5b9015fc6d6355bd
Bergauz <bergauzhrm@gmail.com>
2019-01-08 23:12:46 +0600

    @front #fix api url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -16,7 +16,7 @@ export class ApiService {
   apiURL: string;
 
   constructor(private http: HttpClient, private toastrService: ToastrService) {
-    this.apiURL = 'https://testnet-explorer.gochain.io/api' /*this.getApiURL()*/;
+    this.apiURL = this.getApiURL();
   }
 
   getApiURL() {
