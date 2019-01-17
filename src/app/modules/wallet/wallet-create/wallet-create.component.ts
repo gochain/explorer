6adb58e2efc2786f5116119d7a883f995d4bee44
Bergauz <bergauzhrm@gmail.com>
2019-01-17 23:51:17 +0600

    @front #improvement removed unnecessary step for new account

--- src/app/modules/wallet/wallet-create/wallet-create.component.ts
+++ src/app/modules/wallet/wallet-create/wallet-create.component.ts
@@ -1,19 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WalletService} from '../wallet.service';
+import {Account} from 'web3/eth/accounts';
 
 @Component({
   selector: 'app-wallet-create',
   templateUrl: './wallet-create.component.html',
   styleUrls: ['./wallet-create.component.css']
 })
-export class WalletCreateComponent {
+export class WalletCreateComponent implements OnInit {
 
-  newAccount: any;
+  account: Account;
 
   constructor(private _walletService: WalletService) {
   }
 
-  createAccount(): void {
-    this.newAccount = this._walletService.createAccount();
+  ngOnInit(): void {
+    this.account = this._walletService.createAccount();
   }
 }
