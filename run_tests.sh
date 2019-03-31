fb8b986109123e76515e5ef674713caa4fecc41b
r-gochain <40002763+r-gochain@users.noreply.github.com>
2019-03-31 22:52:35 +0300

    attempt to follow circleci rules

--- run_tests.sh
+++ run_tests.sh
@@ -11,18 +11,20 @@ cleanup_containers() {
     done
 }
 set -e
-trap cleanup_containers EXIT SIGINT SIGTERM
+trap cleanup_containers SIGINT EXIT SIGHUP
 docker run --name test_mongo -d -p 8545:8545 -p 8080:8080 -p 27017:27017 mongo
 varA=`docker ps --no-trunc -q | cut -c 1-12`
 # build
-docker build -t gochain/explorer:test_ci .
+# docker build -t gochain/explorer:test_ci .
 # launch required containers
 docker run --name test_explorer_grabber -d --network="container:$varA" gochain/explorer:test_ci grabber -u https://testnet-rpc.gochain.io -s 10
 docker run --name test_explorer_server -d --network="container:$varA" gochain/explorer:test_ci server -d /explorer/ -u https://testnet-rpc.gochain.io
 # this will run both integration and unit tests, integration test will require mongo running
-echo "Run integration tests"
-docker run --name test_explorer_go_integration -w=/explorer -v $(pwd):/explorer --network="container:$varA" golang:alpine apk add git && go test ./...
-echo "Sleeping"
+docker run --name test_explorer_go_integration -d -w /explorer --network="container:$varA" golang:alpine /bin/sh -c "while true; do sleep 15 ; done"
+docker cp . test_explorer_go_integration:/explorer
+docker exec test_explorer_go_integration apk add git gcc linux-headers g++ make
+docker exec test_explorer_go_integration go test -tags=integration ./...
+
 sleep 20 # let's wait until server start
 # docker exec test_explorer npm test
 echo "Docker logs for grabber"
@@ -33,3 +35,4 @@ echo "Trying curl"
 docker run --rm --network="container:$varA" byrnedo/alpine-curl -f http://localhost:8080/
 docker run --rm --network="container:$varA" byrnedo/alpine-curl -f http://localhost:8080/api/blocks/10
 docker run --rm --network="container:$varA" byrnedo/alpine-curl -f http://localhost:8080/api/blocks/10/transactions
+# cleanup_containers
