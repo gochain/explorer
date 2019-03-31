d75d006013dce77482d735266733f77b4aae7cc7
r-gochain <40002763+r-gochain@users.noreply.github.com>
2019-03-31 18:52:19 +0300

    fixed trap

--- run_tests.sh
+++ run_tests.sh
@@ -11,16 +11,18 @@ cleanup_containers() {
     done
 }
 set -e
-cleanup_containers
+trap cleanup_containers EXIT SIGINT SIGTERM
 docker run --name test_mongo -d -p 8545:8545 -p 8080:8080 -p 27017:27017 mongo
 varA=`docker ps --no-trunc -q | cut -c 1-12`
 # build
-docker build -t gochain/explorer:test_ci .
+# docker build -t gochain/explorer:test_ci .
 # launch required containers
 docker run --name test_explorer_grabber -d --network="container:$varA" gochain/explorer:test_ci grabber -u https://testnet-rpc.gochain.io -s 10
 docker run --name test_explorer_server -d --network="container:$varA" gochain/explorer:test_ci server -d /explorer/ -u https://testnet-rpc.gochain.io
 # this will run both integration and unit tests, integration test will require mongo running
+echo "Run integration tests"
 docker run --name test_explorer_go_integration -w=/explorer -v $(pwd):/explorer --network="container:$varA" golang:alpine apk add git && go test -tags=integration ./...
+echo "Sleeping"
 sleep 20 # let's wait until server start
 # docker exec test_explorer npm test
 echo "Docker logs for grabber"
@@ -31,4 +33,3 @@ echo "Trying curl"
 docker run --rm --network="container:$varA" byrnedo/alpine-curl -f http://localhost:8080/
 docker run --rm --network="container:$varA" byrnedo/alpine-curl -f http://localhost:8080/api/blocks/10
 docker run --rm --network="container:$varA" byrnedo/alpine-curl -f http://localhost:8080/api/blocks/10/transactions
-cleanup_containers
