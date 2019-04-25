e238a25bbd775d89208740e04667d07a7ad31cf5
Bergauz <bergauzhrm@gmail.com>
2019-04-25 16:48:11 +0600

    fixed abi item type

--- server/models/contract.go
+++ server/models/contract.go
@@ -5,25 +5,27 @@ import (
 	"time"
 )
 
-type AbiDefinition struct {
-	Type      string
-	Name      string
-	Constant  bool
-	Anonymous bool
-	Inputs    []abi.Argument
-	Outputs   []abi.Argument
+type AbiItem struct {
+	Anonymous       bool           `json:"anonymous" bson:"anonymous"`
+	Constant        bool           `json:"constant" bson:"constant"`
+	Inputs          []abi.Argument `json:"inputs" bson:"inputs"`
+	Name            string         `json:"name" bson:"name"`
+	Outputs         []abi.Argument `json:"outputs" bson:"outputs"`
+	Payable         bool           `json:"payable" bson:"payable"`
+	StateMutability string         `json:"stateMutability" bson:"stateMutability"`
+	Type            string         `json:"type" bson:"type"`
 }
 
 type Contract struct {
-	Address         string          `json:"address" bson:"address"`
-	Bytecode        string          `json:"byte_code" bson:"byte_code"`
-	Valid           bool            `json:"valid" bson:"valid"`
-	ContractName    string          `json:"contract_name" bson:"contract_name"`
-	CompilerVersion string          `json:"compiler_version" bson:"compiler_version"`
-	Optimization    bool            `json:"optimization" bson:"optimization"`
-	SourceCode      string          `json:"source_code" bson:"source_code"`
-	Abi             []AbiDefinition `json:"abi" bson:"abi"`
-	CreatedAt       time.Time       `json:"created_at" bson:"created_at"`
-	UpdatedAt       time.Time       `json:"updated_at" bson:"updated_at"`
+	Address         string    `json:"address" bson:"address"`
+	Bytecode        string    `json:"byte_code" bson:"byte_code"`
+	Valid           bool      `json:"valid" bson:"valid"`
+	ContractName    string    `json:"contract_name" bson:"contract_name"`
+	CompilerVersion string    `json:"compiler_version" bson:"compiler_version"`
+	Optimization    bool      `json:"optimization" bson:"optimization"`
+	SourceCode      string    `json:"source_code" bson:"source_code"`
+	Abi             []AbiItem `json:"abi" bson:"abi"`
+	CreatedAt       time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at" bson:"updated_at"`
 	/*RecaptchaToken  string    `json:"recaptcha_token"`*/
 }
