6a3b98f8f59bdd954744bde4f27740fdca7e758e
r-gochain <40002763+r-gochain@users.noreply.github.com>
2019-11-17 17:51:55 +0200

    removed unused model

--- server/models/transactions_by_address.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "time"
-
-type TransactionsByAddress struct {
-	TxHash    string    `json:"tx_hash" bson:"tx_hash"`
-	Address   string    `json:"address" bson:"address"`
-	CreatedAt time.Time `json:"created_at" bson:"created_at"`
-}
