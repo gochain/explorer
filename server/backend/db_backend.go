269770d108dc473056f06d62e743f0b014de304e
Travis Reeder <treeder@gmail.com>
2026-07-21 16:17:20 -0700

    Add index hint in getLatestTotalFeesBurned to enforce sparse index usage

--- server/backend/db_backend.go
+++ server/backend/db_backend.go
@@ -524,6 +524,7 @@ func (mb *MongoBackend) getLatestTotalFeesBurned() (*TotalBurned, error) {
 	}{}
 	err := mb.mongo.C("Blocks").
 		Find(bson.M{"total_fees_burned": bson.M{"$gt": ""}}). // not null or empty
+		Hint("total_fees_burned", "-created_at").
 		Sort("-created_at").
 		Select(bson.M{"number": 1, "total_fees_burned": 1}).One(&v)
 	if err != nil {
